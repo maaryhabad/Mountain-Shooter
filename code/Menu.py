deb2d3ae17a209a35ab7cf0cdb8cad907c5bb56c
Mariana Beilune Abad <12502572+maaryhabad@users.noreply.github.com>
2025-08-26 15:51:30 -0300

    added new informations and music to level scene, and added convert_alpha in images loaded

--- code/Menu.py
+++ code/Menu.py
@@ -8,7 +8,7 @@ from code.Const import WIN_WIDTH, COLOR_YELLOW_DARK, MENU_OPTION, COLOR_BLUE, CO
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./assets/MenuBg.png')
+        self.surf = pygame.image.load('./assets/MenuBg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0,top=0)
 
     def run(self, ):
