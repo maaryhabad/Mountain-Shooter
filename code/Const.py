e509ddeab40072222baec3609bb21fdbb12c51ba
Mariana Beilune Abad <12502572+maaryhabad@users.noreply.github.com>
2025-08-25 22:15:32 -0300

    Creating assets folder, and setting up the menu

---
+++ code/Const.py
@@ -0,0 +1,15 @@
+# C
+COLOR_YELLOW_DARK = (182, 134, 0)
+COLOR_YELLOW = (255, 202, 22)
+COLOR_BLUE = (73, 213, 255)
+
+# M
+MENU_OPTION = ( 'NEW GAME 1P',
+                'NEW GAME 2P - COOPERATIVE',
+                'NEW GAME 2P - COMPETITIVE',
+                'SCORE'
+                'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/Menu.py
+++ code/Menu.py
@@ -1,9 +1,37 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+
+from code.Const import WIN_WIDTH, COLOR_YELLOW_DARK, MENU_OPTION, COLOR_BLUE
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./assets/MenuBg.png')
+        self.rect = self.surf.get_rect(left=0,top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./assets/Menu.mp3')
+        pygame.mixer_music.play(-1)
+
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, 'Mountain', COLOR_YELLOW_DARK, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, 'Shooter', COLOR_YELLOW_DARK, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_BLUE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font = pygame.font.SysFont('Phosphate', text_size)
+        text_surf = text_font.render(text, True, text_color).convert_alpha()
+        text_rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(text_surf, text_rect)
