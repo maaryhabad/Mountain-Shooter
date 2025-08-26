f3100bd7138abd36f835a05693bdb653461c60b2
Mariana Beilune Abad <12502572+maaryhabad@users.noreply.github.com>
2025-08-26 16:46:23 -0300

    creating and moving players and enemys

--- code/Const.py
+++ code/Const.py
@@ -1,9 +1,12 @@
 # C
+import pygame
+
 COLOR_BLUE = (73, 213, 255)
 COLOR_YELLOW = (255, 202, 22)
 COLOR_YELLOW_DARK = (182, 134, 0)
 COLOR_WHITE = (255, 255, 255)
 
+#E
 ENTITY_SPEED = {
     'Level1Bg0' : 0,
     'Level1Bg1' : 1,
@@ -12,7 +15,12 @@ ENTITY_SPEED = {
     'Level1Bg4' : 4,
     'Level1Bg5' : 5,
     'Level1Bg6' : 6,
+    'Player1' : 3,
+    'Player2' : 3,
+    'Enemy1' : 2,
+    'Enemy2' : 1
 }
+EVENT_ENEMY = pygame.USEREVENT + 1
 
 # M
 MENU_OPTION = ( 'NEW GAME 1P',
@@ -20,7 +28,15 @@ MENU_OPTION = ( 'NEW GAME 1P',
                 'NEW GAME 2P - COMPETITIVE',
                 'SCORE',
                 'EXIT')
+# P
+PLAYER_KEY_UP = {'Player1': pygame.K_UP, 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN, 'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT, 'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT, 'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL, 'Player2': pygame.K_LCTRL}
 
+# S
+SPAWN_TIME = 2000
 # W
-WIN_WIDTH = 576
 WIN_HEIGHT = 324
+WIN_WIDTH = 576
--- code/Level.py
+++ code/Level.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
+
 import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import COLOR_WHITE, WIN_HEIGHT
+from code.Const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
+
 
 
 class Level:
@@ -16,7 +19,13 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('level1Bg'))
+        self.entity_list.append(EntityFactory.get_entity('Player1'))
         self.timeout = 20000 # 20 segundos
+
+        if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity('Player2'))
+
+        pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
 
     def run(self, ):
         pygame.mixer_music.load(f'assets/{self.name}.mp3')
@@ -31,6 +40,9 @@ class Level:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
+                if event.type == EVENT_ENEMY:
+                    choice = random.choice(('Enemy1', 'Enemy2'))
+                    self.entity_list.append(EntityFactory.get_entity(choice))
 
             # printed text
             self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000 : .1f}s', COLOR_WHITE, (10, 5))
