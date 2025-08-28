3c9b8b13eea0e1d8b6fb9276b906c6e23e2d32ac
Mariana Beilune Abad <12502572+maaryhabad@users.noreply.github.com>
2025-08-27 21:46:55 -0300

    Creating player and enemy shot

--- code/Player.py
+++ code/Player.py
@@ -3,11 +3,13 @@
 
 from code.Const import *
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
 
 
 class Player(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
     def move(self, ):
         pressed_key = pygame.key.get_pressed()
@@ -20,3 +22,11 @@ class Player(Entity):
         if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH:
                 self.rect.centerx += ENTITY_SPEED[self.name]
         pass
+
+    def shot(self):
+        self.shot_delay -= 1
+        if self.shot_delay < 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            pressed_key = pygame.key.get_pressed()
+            if pressed_key[PLAYER_KEY_SHOT[self.name]]:
+                return PlayerShot('Player1Shot', (self.rect.centerx, self.rect.centery))
